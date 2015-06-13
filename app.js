5015bf2c64d38b50895140107c581f0f629783c8
Oliver Pope <opope@me.com>
2015-06-13 16:42:20 -0400

    fixing

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('node-compass')({mode: 'expanded'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -56,5 +55,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
+
+app.listen(5000);
 
 module.exports = app;
